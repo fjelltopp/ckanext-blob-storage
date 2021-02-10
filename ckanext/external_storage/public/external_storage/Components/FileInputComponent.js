c20e4e78c84e30f7870a237e63d7a37ce195b536
manoj-nathwani <manoj_1993@hotmail.com>
2021-02-10 13:24:13 +0000

    cleaned up js code

--- ckanext/external_storage/public/external_storage/Components/FileInputComponent.js
+++ ckanext/external_storage/public/external_storage/Components/FileInputComponent.js
@@ -4,21 +4,7 @@ import { Client } from "giftless-client";
 import { open } from "frictionless.js";
 import axios from "axios";
 
-const element = document.getElementById('FileInputComponent');
-const getAttr = key => {
-  const val = element.getAttribute(`data-${key}`);
-  return val in ['None', ''] ? null : val;
-};
-const requiredString = str => {
-  console.assert(str.length);
-  return str;
-}
-const
-  lfsServer = requiredString(getAttr('lfsServer')),
-  orgId = requiredString(getAttr('orgId')),
-  datasetId = requiredString(getAttr('datasetId'));
-
-function App() {
+function App({ lfsServer, orgId, datasetId }) {
   const [authToken, setAuthToken] = useState();
   const [uploadProgress, setUploadProgress] = useState({
     loaded: 0, total: 0
@@ -27,6 +13,7 @@ function App() {
     sha256: null, name: null, size: null
   })
 
+  // fetch authToken from ckan authz_authorize
   if (!authToken) {
     axios.post(
       'http://adr/api/3/action/authz_authorize',
@@ -43,37 +30,26 @@ function App() {
     return 'Authentication Error: Failed to load file uploader';
   }
 
-  const lfsPrefix = [orgId, datasetId].join('/');
-  const client = new Client(lfsServer, authToken, ['basic']);
-
-  function onProgress(progress) {
-    setUploadProgress({
-      loaded: progress.loaded,
-      total: progress.total
-    });
-  }
-
-  const handleClick = async inputFiles => {
-    if (!inputFiles[0]) return;
-    const file = open(inputFiles[0]);
-    await client.upload(file, orgId, datasetId, onProgress);
-    setUploadProgress({ loaded: 100, total: 100 });
-    setHiddenInputs({
-      sha256: file._computedHashes.sha256,
-      name: file._descriptor.name,
-      size: file._descriptor.size,
-    })
-  }
-
-  const inputs = [
-    { name: 'url_type', value: 'upload' },
-    { name: 'lfs_prefix', value: lfsPrefix },
-    { name: 'url', value: hiddenInputs.name },
-    { name: 'sha256', value: hiddenInputs.sha256 },
-    { name: 'size', value: hiddenInputs.size }
-  ];
-
+  // return file input form
   if (uploadProgress.total == 0) {
+    const handleClick = async inputFiles => {
+      if (!inputFiles[0]) return;
+      const file = open(inputFiles[0]);
+      const client = new Client(lfsServer, authToken, ['basic']);
+      await client.upload(file, orgId, datasetId, onProgress);
+      setUploadProgress({ loaded: 100, total: 100 });
+      setHiddenInputs({
+        sha256: file._computedHashes.sha256,
+        name: file._descriptor.name,
+        size: file._descriptor.size,
+      })
+    }
+    function onProgress(progress) {
+      setUploadProgress({
+        loaded: progress.loaded,
+        total: progress.total
+      });
+    }
     return (
       <div className="image-upload">
         <div className="form-group control-full">
@@ -92,54 +68,78 @@ function App() {
         </div>
       </div>
     )
-  } else {    
-    const progressBar = () => {
-      const percent = Math.round(
-        (uploadProgress.loaded / uploadProgress.total) * 100
-      );
-      if (percent < 100 || !hiddenInputs.name){
-        return (
-          <div className="form-group controls progress progress-striped active">
-            <div
-              className="progress-bar"
-              style={{ width: `${percent}%` }}
-            >
-              <span>{percent}%</span>
-            </div>
-          </div>
-        )
-      }else{
-        return (
-          <div className="form-group controls progress">
-            <div
-              className="progress-bar progress-bar-success"
-              style={{ width: `${percent}%` }}
-            >
-              <span><i className="fa fa-file"></i>&nbsp;{hiddenInputs.name}</span>
-            </div>
-          </div>
-        )
-      }
-    }
-    return (
-      <>
-        {progressBar()}
-        {inputs.map(input =>
-          <input
-            key={input.name}
-            name={input.name}
-            value={input.value || ''}
-            type="hidden"
-          />
-        )}
-      </>
-    );
   }
 
+  // return progress bar
+  const progressBar = () => {
+    const percent = Math.round(
+      (uploadProgress.loaded / uploadProgress.total) * 100
+    );
+    return percent < 100 || !hiddenInputs.name
+      ? (
+        <div className="form-group controls progress progress-striped active">
+          <div
+            className="progress-bar"
+            style={{ width: `${percent}%` }}
+          >
+            <span>{percent}%</span>
+          </div>
+        </div>
+      )
+      : (
+        <div className="form-group controls progress">
+          <div
+            className="progress-bar progress-bar-success"
+            style={{ width: `${percent}%` }}
+          >
+            <span><i className="fa fa-file"></i>&nbsp;{hiddenInputs.name}</span>
+          </div>
+        </div>
+      )
+  }
+  const inputs = [
+    { name: 'url_type', value: 'upload' },
+    { name: 'lfs_prefix', value: [orgId, datasetId].join('/') },
+    { name: 'url', value: hiddenInputs.name },
+    { name: 'sha256', value: hiddenInputs.sha256 },
+    { name: 'size', value: hiddenInputs.size }
+  ];
+  return (
+    <>
+      {progressBar()}
+      {inputs.map(input =>
+        <input
+          key={input.name}
+          name={input.name}
+          value={input.value || ''}
+          type="hidden"
+        />
+      )}
+    </>
+  );
 
 }
 
+const componentElement = 
+  document.getElementById('FileInputComponent');
+const getAttr = key => {
+  const val = componentElement.getAttribute(`data-${key}`);
+  return val in ['None', ''] ? null : val;
+};
+const requiredString = str => {
+  console.assert(str.length);
+  return str;
+}
+const
+  lfsServer = requiredString(getAttr('lfsServer')),
+  orgId = requiredString(getAttr('orgId')),
+  datasetId = requiredString(getAttr('datasetId'));
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('FileInputComponent')
+  <App
+    lfsServer={lfsServer}
+    orgId={orgId}
+    datasetId={datasetId}
+  />,
+  componentElement
 );
