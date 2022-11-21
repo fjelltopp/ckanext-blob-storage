5754d9160443e0bb873985f602db24322b8a8fae
toavina <toavina@fjelltopp.org>
2022-11-21 16:47:10 +0300

    fix in auhtz.py (condition too long) | https://github.com/fjelltopp/ckanext-blob-storage/pull/12#discussion_r1027949553

--- ckanext/blob_storage/authz.py
+++ ckanext/blob_storage/authz.py
@@ -122,8 +122,9 @@ def check_resource_permissions(id, dataset_id=None, organization_id=None, activi
         # Resource permissions for "all resources" can be taken from dataset permissions
         return granted.intersection(set(RES_ENTITY_CHECKS.keys()))
 
-    if not helpers.find_activity_package_and_resource(activity_id, id, dataset_id, context=context) and \
-            not helpers.check_resource_in_dataset(resource_id=id, dataset_id=dataset_id, context=context):
+    resource_found = helpers.find_activity_package_and_resource(activity_id, id, dataset_id, context=context)
+    resource_in_dataset = helpers.check_resource_in_dataset(resource_id=id, dataset_id=dataset_id, context=context)
+    if not resource_found and not resource_in_dataset:
         return set()
 
     return check_entity_permissions(RES_ENTITY_CHECKS, {"id": id}, context=context)
